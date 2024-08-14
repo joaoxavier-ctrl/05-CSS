21e456206b963af1b6ebcaf4f7ac25c445190eb4
JoaoXavierWin <joao.xavier21@gmail.com>
2024-08-14 16:42:00 -0300

    aula css-frameworks finalizada

--- 0504-css-frameworks/src/App.jsx
+++ 0504-css-frameworks/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
-  return <div>App reacts</div>;
+  //return <div className='card bg-dark text-white m-5' style={{maxWidth:'18rem'}}>
+  //  <div className='card-header'>Notebook</div>
+  //  <div className='card-body'>
+  //    <h5 className='card-title'>R$ 2500</h5>
+  //    <p className='card-text'>Esse é um Notebook com 16gb e placa de video top, confia.</p>
+  //  </div>
+  //</div>;
+  return <Card bg='dark' text='white' className='m-5' style={{maxWidth: '18rem'}}>
+    <Card.Header>Notebook</Card.Header>
+    <Card.Body>
+      <Card.Title>R$ 2500</Card.Title>
+      <Card.Text>abluea sdluasldbnj sakdjhk asbnasdu</Card.Text>
+    </Card.Body>
+  </Card>
 }
 
 export default App
